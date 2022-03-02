8a709b790ceaf8d3b49a9783b37602a4e8b3786b
Uncle Scientist <unclescientist@gmail.com>
2022-03-02 07:46:15 -0500

    don't print double quotes around strings

--- src/object.rs
+++ src/object.rs
@@ -14,7 +14,7 @@ impl fmt::Display for Object {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Object::Num(x) => write!(f, "{x}"),
-            Object::Str(x) => write!(f, "\"{x}\""),
+            Object::Str(x) => write!(f, "{x}"),
             Object::Bool(x) => {
                 if *x {
                     write!(f, "true")
